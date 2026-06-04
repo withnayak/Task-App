b440945b891dab9ae2c383705e1d99da2ae51e56
Vikash Kumar <withnayak.dev@gmail.com>
2026-06-04 17:23:02 +0530

    in header section style  users

--- src/components/Others/Headers.jsx
+++ src/components/Others/Headers.jsx
@@ -4,8 +4,8 @@ const Headers = () => {
   return (
     <div className='flex items-end justify-between '>
 
-        <h1 className='text-white font-medium text-2xl'>
-            Hello,<br /><span className='font-bold text-3xl'>Nayak 👋</span> 
+        <h1 className='text-white font-medium text-3xl'>
+            Hello,<br /><span className='font-bold text-5xl'>Nayak 👋</span> 
         </h1>
 
         <button className='text-white font-medium text-lg px-5 py-3 rounded-lg bg-red-700 hover:bg-red-800 cursor-pointer transition duration-300'>Log Out</button>
